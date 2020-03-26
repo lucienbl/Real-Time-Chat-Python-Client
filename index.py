28039218395b96c05fbb76a8a58c8dfc90148533
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2020-03-26 18:55:41 +0100

    feat: added event dispatcher

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 import socketio
+from pyeventdispatcher import register
 from src.ChatSocketHandler import ChatSocketHandler
 
 # Connect to the gateway
 sio = socketio.Client()
 sio.connect('http://localhost:5000', namespaces=['/chat'])
 print('Connected! SID:', sio.sid)
+
+# Event listeners
+register("event_message_add", lambda event: print(f"{event.data['user_id']}::{event.data['message']}"))
 
 # Register namespaces
 sio.register_namespace(ChatSocketHandler('/chat'))
--- src/ChatSocketHandler.py
+++ src/ChatSocketHandler.py
@@ -1,14 +1,15 @@
 import socketio
+from pyeventdispatcher import dispatch, Event
 
 
 class ChatSocketHandler(socketio.ClientNamespace):
     def on_connect(self):
         self.emit('join', {'user_id': "1", 'room_id': '1'})
-        self.emit('message_add', {'room_id': '1', 'message': "Hello!!"})
+        self.emit('message_add', {'room_id': '1', 'user_id': '1', 'message': "Hello!!"})
 
     def on_disconnect(self):
         pass
 
     @staticmethod
     def on_message_add(data):
-        print(str(data))
+        dispatch(Event("event_message_add", data))
